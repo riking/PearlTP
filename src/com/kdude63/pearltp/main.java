cb22bba492b8fea330bf58896b1b4eaa7bd20958
Keith Gadberry <me@kdude63.com>
2013-07-07 15:15:23 -0700

    Completely redone. Almost works.

--- src/com/kdude63/pearltp/main.java
+++ src/com/kdude63/pearltp/main.java
@@ -1,71 +1,107 @@
 package com.kdude63.pearltp;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.*;
 import org.bukkit.entity.*;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class main extends JavaPlugin implements Listener
 {
 	Logger log = Logger.getLogger("Minecraft");
 	FileConfiguration config;
+	Integer cost;
+	Integer maxdist;
+	String noperm;
+	Boolean itp;
 	
 	@Override
 	public void onEnable()
 	{
 		getServer().getPluginManager().registerEvents(this, this);
+		config = getConfig();
+        if (!new File(this.getDataFolder().getPath() + File.separatorChar + "config.yml").exists())
+            saveDefaultConfig();
+        
+        cost = config.getInt("cost");
+        maxdist = config.getInt("max_distance");
+        noperm = config.getString("noperm_message");
+        itp = config.getBoolean("itp");
+	}
+	
+	public boolean isNumber(String n){
+		if (n.matches("-?\\d+(\\.\\d+)?"))
+			return true;
+		else
+			return false;
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if (cmd.getName().equalsIgnoreCase("tp")){
-			if (!(sender instanceof Player)) { //If the command sender is console
-				if (args.length <= 1 || args.length > 4 || args.length == 3) {; //If the command does not have either two or four arguments
-					return false; //Print proper command usage
-				}else{
-					if (args.length == 2){ //If command only has two arguments
-						if (Bukkit.getServer().getPlayer(args[0]) != null){ //If first argument in command is an existing player
-							if (Bukkit.getServer().getPlayer(args[1]) != null){ //If second argument in command is an existing player
-								Player player = Bukkit.getServer().getPlayer(args[0]); //Store origin player in variable
-								Location target = Bukkit.getServer().getPlayer(args[1]).getLocation(); //Store target player location in variable
-								player.teleport(target); //Teleport origin player to target player's location			
+		if (cmd.getName().equalsIgnoreCase("ptp")){
+			if (sender instanceof Player){ //If the command sender is a player
+				if (sender.hasPermission("pearltp.notp")){
+					sender.sendMessage(noperm);	
+				}
+				else
+				{
+					if (args.length == 1 || args.length == 3){
+						if (args.length == 1){ //If the command has one argument
+							Player player = Bukkit.getServer().getPlayer(sender.getName());
+							Location target = Bukkit.getServer().getPlayer(args[0]).getLocation();
+							Inventory pinv = player.getInventory();
+							ItemStack pearls = new ItemStack(Material.ENDER_PEARL, cost);
+							if (itp){						
+								if (pinv.contains(Material.ENDER_PEARL, cost)){
+									pinv.remove(pearls);
+									player.teleport(target);
+								}else{
+									sender.sendMessage(ChatColor.RED + "You need " + cost.toString() + " pearl(s) to teleport." + ChatColor.RESET);
+								}
 							}else{
-								System.out.println("Cannot find player " + args[1]); //Tell the console that the target player is nonexistent
+								sender.sendMessage("You can't teleport to other players.");
 							}
-						}else{
-							System.out.println("Cannot find player " + args[0]); //Tell the console that the origin player is nonexistent
-							return true;
 						}
-					} else if (args.length == 4){ //If the command has four arguments
-						if (Bukkit.getServer().getPlayer(args[0]) != null){
-							if (args[1].matches("-?\\d+(\\.\\d+)?") && args[2].matches("-?\\d+(\\.\\d+)?") && args[3].matches("-?\\d+(\\.\\d+)?")){					
-								Player player = Bukkit.getServer().getPlayer(args[0]); //Store origin player in variable
+						else if (args.length == 3){ //If the command has three arguments
+							if (isNumber(args[0]) && isNumber(args[1]) && isNumber(args[2])){
+								Player player = Bukkit.getServer().getPlayer(sender.getName());
 								Location target = player.getLocation();
-								
-								//Set the target coordinates
-								target.setX(Double.parseDouble(args[1]));
-								target.setY(Double.parseDouble(args[2]));
-								target.setZ(Double.parseDouble(args[3]));
-								
-								player.teleport(target); //Teleport the origin player to the target coordinates
+								Inventory pinv = player.getInventory();
+								ItemStack pearls = new ItemStack(Material.ENDER_PEARL, cost);
+								if (pinv.contains(Material.ENDER_PEARL, cost)){
+									target.setX(Double.parseDouble(args[0]));
+									target.setY(Double.parseDouble(args[1]));
+									target.setZ(Double.parseDouble(args[2]));
+									
+									pinv.remove(pearls);
+									player.teleport(target);
+								}else{
+									sender.sendMessage(ChatColor.RED + "You need " + cost.toString() + " pearl(s) to teleport." + ChatColor.RESET);
+								}
 							}else{
-								return false; //Print proper command usage
+								return false;
 							}
-						}else{
-							System.out.println("Could not find player " + args[0]); //Tell the console that the origin player is nonexistent
 						}
 					}
+					else{
+						return false;
+					}
 				}
-						
-			}else{ //If the command sender is a player
-				
+			}
+			else{ //If the command sender is console
+				sender.sendMessage("Console can't use this command!");
 			}
 			return true;
-		}else{
+		}
+		else{
 			return false;
 		}
 	}
